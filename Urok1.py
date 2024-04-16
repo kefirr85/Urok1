0dea1f253d6aee7bf720dc14436cae2fdbe4eb03
kefirr85 <166715341+kefirr85@users.noreply.github.com>
2024-04-16 20:14:26 +0300

    Add files via upload

---
+++ Urok1.py
@@ -0,0 +1,9 @@
+name1 = 'это моя строка'
+print(name1[0])
+print(name1[-1])
+print(name1[2:5])
+print(name1[::-1])
+print(len('это моя строка'))
+name2 = 'это новая строка'
+print(name2 + name1)
+print('это новая строка'+'это моя строка')
